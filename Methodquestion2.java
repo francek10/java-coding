622d823ff90c2251828904d65fd2dfcd3b6ec53b
francek10 <franciswaithaka2005@gmail.com>
2025-12-16 11:38:47 +0300

    Create Methodquestion2.java

---
+++ Methodquestion2.java
@@ -0,0 +1,27 @@
+//Francis Waithaka Wainaina J77-11170-2024
+
+//Write amethod getFitnessLevel that takes an integer steps and return 
+import java.util.Scanner;
+public class FitnessTracker {
+
+    //Method to determine fitness level
+    public static String getFitnessLevel(int steps) {
+        if (steps >= 10000) {
+            return "Excellent";
+        } else if (steps >= 7000) {
+            return "Good";
+        } else if (steps >= 4000) {
+            return "Average";
+        } else {
+            return "Poor";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your steps for today: ");
+        int steps = scanner.nextInt();
+        String fitnessLevel = getFitnessLevel(steps);
+        System.out.println("Your fitness level: " + fitnessLevel);
+    }
